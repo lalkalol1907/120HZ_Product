c469ef502fe81431a160d2e3b0fc92c1fb4d9cb1
lalkalol1907 <kostyaleonov13@yandex.ru>
2020-02-03 14:26:21 +0300

    Change Java Project structure

--- VKBot/src/main/java/VK/Event.java
+++
@@ -1,4 +0,0 @@
-package VK;
-
-public class Event {
-}
--- VKBot/src/main/java/VK/VKConfig.java
+++
@@ -1,6 +0,0 @@
-package VK;
-
-public class VKConfig {
-    public static int groupId() {return 189697309;}
-    public static String access_token() {return "abb24c63f5cc50b1cced3ef84d1007cc0954f833cf58f37fe376774c6313a266900464f51747157246e1c";}
-}
